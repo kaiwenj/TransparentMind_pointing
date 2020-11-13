867a32be5d3a9b4ff8c7e907b92278d00248219f
kaiwenj <64115446+kaiwenj@users.noreply.github.com>
2020-11-12 16:09:11 -0800

    Create POMDPSimulator.py

---
+++ src/POMDPSimulator.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class SimulatePOMDP(object):
+    
+    def __init__(self, stateSpace, actionSpace, observationSpace, transitionFunction, rewardFunction, observationFunction):
+        self.stateSpace=stateSpace
+        self.actionSpace=actionSpace
+        self.observationSpace=observationSpace
+        self.transitionFunction=transitionFunction
+        self.rewardFunction=rewardFunction
+        self.observationFunction=observationFunction
+    
+    def __call__(self, s, a):
+        sPrimeDistribution={sPrime: self.transitionFunction(s, a, sPrime) for sPrime in self.stateSpace}
+        sPrime=np.random.choice(list(sPrimeDistribution.keys()), p=list(sPrimeDistribution.values()))
+        r=self.rewardFunction(s, a, sPrime)
+        oDistribution={o: self.observationFunction(sPrime, a, o) for o in self.observationSpace}
+        o=np.random.choices(list(oDistribution.keys()), p=list(oDistribution.values()))
+        return (sPrime, r, o)
